57abf3431a965c91f7b37093c394d193da3932a9
Sirli Kont <sirlikont@gmail.com>
2025-06-05 23:36:00 +0300

    Autoloading, Namespaces, and Models

---
+++ app/Models/Job.php
@@ -0,0 +1,22 @@
+<?php
+namespace App\Models;
+use Illuminate\Support\Arr;
+
+class Job {
+    public static function all(): array {
+        return [
+            ['id'=> 1, 'title' => 'Software Engineer', 'salary' => '50000'],
+            ['id'=> 2, 'title' => 'Data Analyst', 'salary' => '40000'],
+            ['id'=> 3, 'title' => 'Web Developer', 'salary' => '50000'],
+        ];
+    }
+
+    public static function find(int $id): array {
+        $job = Arr::first(static::all(), fn($job) => $job['id'] == $id);
+        if(! $job){
+            abort(404);
+        }
+        return $job;
+    }
+
+}
