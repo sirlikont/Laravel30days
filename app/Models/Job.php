9e7b35c90d6524950f070680d55989036a5f3aec
Sirli Kont <sirlikont@gmail.com>
2025-06-06 19:20:55 +0300

    Meet Eloquent

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -1,22 +1,15 @@
 <?php
 namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Arr;
 
-class Job {
-    public static function all(): array {
-        return [
-            ['id'=> 1, 'title' => 'Software Engineer', 'salary' => '50000'],
-            ['id'=> 2, 'title' => 'Data Analyst', 'salary' => '40000'],
-            ['id'=> 3, 'title' => 'Web Developer', 'salary' => '50000'],
-        ];
-    }
+class Job extends Model{
+    protected $table = 'job_listings';
 
-    public static function find(int $id): array {
-        $job = Arr::first(static::all(), fn($job) => $job['id'] == $id);
-        if(! $job){
-            abort(404);
-        }
-        return $job;
-    }
+    protected $fillable = [
+        'title',
+        'salary',
+    ];
 
 }
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Models\Job;
 
 Route::get('/', function () {
+
     return view('home');
 });
 
