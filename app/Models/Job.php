e57b805cc7d6ff6b2248eac2ad20090cff13ea0d
Sirli Kont <sirlikont@gmail.com>
2025-06-06 20:32:35 +0300

    factories

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -2,9 +2,12 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Support\Arr;
 
 class Job extends Model{
+    use HasFactory;
+
     protected $table = 'job_listings';
 
     protected $fillable = [
--- resources/views/jobs.blade.php
+++ resources/views/jobs.blade.php
@@ -7,8 +7,7 @@
         @foreach ( $jobs as $job )
             <li>
                 <a href="/jobs/{{ $job['id'] }}" class="text-blue-500 hover:text-blue-700">
-                    <strong>{{ $job['title'] }}</strong>
-                    <strong>{{ $job['title'] }}:</strong> pays $ {{ $job['salary'] }} per year.
+                    <strong>{{ $job->title }}</strong>: pays $ {{ $job->salary }} per year.
                 </a>
             </li>
         @endforeach
