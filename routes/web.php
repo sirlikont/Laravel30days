e15f55aad24abbf4201d58379286da97670ec105
Sirli Kont <sirlikont@gmail.com>
2025-06-07 23:37:10 +0300

    route resource

--- routes/web.php
+++ routes/web.php
@@ -5,14 +5,16 @@ use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'home');
 
-Route::controller('App\Http\Controllers\JobController')->group(function () {
-    Route::get('/jobs', 'index');
-    Route::get('/jobs/create', 'create');
-    Route::get('/jobs/{job}', 'show');
-    Route::post('/jobs', 'store');
-    Route::get('/jobs/{job}/edit', 'edit');
-    Route::patch('/jobs/{job}', 'update');
-    Route::delete('/jobs/{job}', 'destroy');
-});
+//Route::controller('App\Http\Controllers\JobController')->group(function () {
+//    Route::get('/jobs', 'index');
+//    Route::get('/jobs/create', 'create');
+//    Route::get('/jobs/{job}', 'show');
+//    Route::post('/jobs', 'store');
+//    Route::get('/jobs/{job}/edit', 'edit');
+//    Route::patch('/jobs/{job}', 'update');
+//    Route::delete('/jobs/{job}', 'destroy');
+//});
+
+Route::resource('jobs', JobController::class);
 
 Route::view('/contact', 'contact');
