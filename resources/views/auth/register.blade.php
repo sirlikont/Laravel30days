2b85228c233479e8a1736c4acaeafe4968e1296e
Sirli Kont <sirlikont@gmail.com>
2025-06-08 17:39:49 +0300

    login and registration

---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,85 @@
+<x-layout>
+    <x-slot:heading>
+        Register
+    </x-slot:heading>
+
+    <form method='POST' action='/register'>
+        @csrf
+        <div class="space-y-12">
+            <div class="border-b border-gray-900/10 pb-12">
+
+                <div class="grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+
+                    <x-form-field>
+                        <x-form-label for='first_name'>First Name</x-form-label>
+
+                        <div class="mt-2">
+
+                            <x-form-input name="first_name" id="first_name"  placeholder="First Name" required />
+
+                            <x-form-error name='first_name' />
+
+                        </div>
+                    </x-form-field>
+
+                    <x-form-field>
+                        <x-form-label for='last_name'>Last Name</x-form-label>
+
+                        <div class="mt-2">
+
+                            <x-form-input name="last_name" id="last_name"  placeholder="Last Name" required />
+
+                            <x-form-error name='last_name' />
+
+                        </div>
+                    </x-form-field>
+
+                    <x-form-field>
+                        <x-form-label for='email'>Email</x-form-label>
+
+                        <div class="mt-2">
+
+                            <x-form-input name="email" id="email"  placeholder="Email" required />
+
+                            <x-form-error name='email' />
+
+                        </div>
+                    </x-form-field>
+
+                    <x-form-field>
+                        <x-form-label for='password'>Password</x-form-label>
+
+                        <div class="mt-2">
+
+                            <x-form-input type="password" name="password" id="password"  placeholder="Password" required />
+
+                            <x-form-error name='password' />
+
+                        </div>
+                    </x-form-field>
+
+                    <x-form-field>
+                        <x-form-label for='password_confirmation'>Confirm Password</x-form-label>
+
+                        <div class="mt-2">
+
+                            <x-form-input type="password_confirmation" name="password_confirmation" id="password_confirmation"  placeholder="Confirm Password" required />
+
+                            <x-form-error name='password_confirmation' />
+
+                        </div>
+                    </x-form-field>
+
+                </div>
+
+            </div>
+        </div>
+
+        <div class="mt-6 flex items-center justify-end gap-x-6">
+            <a href="/" type="button" class="text-sm/6 font-semibold text-gray-900">Cancel</a>
+            <x-form-button>Register</x-form-button>
+        </div>
+
+    </form>
+
+</x-layout>
--- routes/web.php
+++ routes/web.php
@@ -2,19 +2,17 @@
 
 use App\Http\Controllers\JobController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\RegisteredUserController;
+use App\Http\Controllers\SessionController;
+use Illuminate\Contracts\Session\Session;
 
 Route::view('/', 'home');
-
-//Route::controller('App\Http\Controllers\JobController')->group(function () {
-//    Route::get('/jobs', 'index');
-//    Route::get('/jobs/create', 'create');
-//    Route::get('/jobs/{job}', 'show');
-//    Route::post('/jobs', 'store');
-//    Route::get('/jobs/{job}/edit', 'edit');
-//    Route::patch('/jobs/{job}', 'update');
-//    Route::delete('/jobs/{job}', 'destroy');
-//});
+Route::view('/contact', 'contact');
 
 Route::resource('jobs', JobController::class);
+//Auth
+Route::get('/register',[RegisteredUserController::class, 'create']);
+Route::post('/register', [RegisteredUserController::class, 'store']);
 
-Route::view('/contact', 'contact');
+Route::get('/login', [SessionController::class, 'create']);
+Route::post('/login', [SessionController::class, 'store']);
