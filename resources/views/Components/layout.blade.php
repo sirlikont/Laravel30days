276e85a987b0c255db8aa6a661c62b983f1b936e
Sirli Kont <sirlikont@gmail.com>
2025-06-09 17:15:57 +0300

    fix local TailwindCSS and config

--- resources/views/Components/layout.blade.php
+++ resources/views/Components/layout.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home</title>
     @vite(['resources/js/app.js', 'resources/css/app.css'])
-    <script src="https://cdn.tailwindcss.com"></script>
+    <!--<script src="https://cdn.tailwindcss.com"></script>-->
 </head>
 <body class="h-full">
 
